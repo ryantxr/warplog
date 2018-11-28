e5027e7b992fdcb1f33cbf5e115d80bdd446ee42
ryantxr <ryantxr@gmail.com>
2018-11-27 22:45:22 -0500

    Fix log convenience functions

--- src/WarpLog.php
+++ src/WarpLog.php
@@ -7,33 +7,33 @@ class WarpLog
     {
         self::$logger = ($logger) ? $logger : new \Psr\Log\NullLogger;
     }
-    static public function debug($msg, $context)
+    static public function debug($msg, array $context = [])
     {
         self::$logger->debug($msg, $context);
     }
-    static public function notice($msg, $context)
+    static public function notice($msg, array $context = [])
     {
         self::$logger->notice($msg, $context);        
     }
-    static public function warning($msg, $context)
+    static public function warning($msg, array $context = [])
     {
         self::$logger->warning($msg, $context);
     }
-    static public function error($msg, $context)
+    static public function error($msg, array $context = [])
     {
         self::$logger->error($msg, $context);
     }
-    static public function critical($msg, $context)
+    static public function critical($msg, array $context = [])
     {
         self::$logger->decriticalbug($msg, $context);
     }
 
-    static public function alert($msg, $context)
+    static public function alert($msg, array $context = [])
     {
         self::$logger->alert($msg, $context);
     }
 
-    static public function emergency($msg, $context)
+    static public function emergency($msg, array $context = [])
     {
         self::$logger->emergency($msg, $context);
     }
