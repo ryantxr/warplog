06c37174d140e5b0db97c429d395e080b7d7789d
ryantxr <ryantxr@gmail.com>
2018-12-06 16:51:05 -0500

    Set up to use null logger if no logger is specified.

--- src/WarpLog.php
+++ src/WarpLog.php
@@ -3,38 +3,59 @@
 class WarpLog
 {
     public static $logger;
-    static public function init($logger=null)
+    static public function init(\Psr\Log\LoggerInterface $logger=null)
     {
         self::$logger = ($logger) ? $logger : new \Psr\Log\NullLogger;
     }
     static public function debug($msg, array $context = [])
     {
+        if ( ! self::$logger ) {
+            self::init(null);
+        }
         self::$logger->debug($msg, $context);
     }
     static public function notice($msg, array $context = [])
     {
+        if ( ! self::$logger ) {
+            self::init(null);
+        }
         self::$logger->notice($msg, $context);        
     }
     static public function warning($msg, array $context = [])
     {
+        if ( ! self::$logger ) {
+            self::init(null);
+        }
         self::$logger->warning($msg, $context);
     }
     static public function error($msg, array $context = [])
     {
+        if ( ! self::$logger ) {
+            self::init(null);
+        }
         self::$logger->error($msg, $context);
     }
     static public function critical($msg, array $context = [])
     {
-        self::$logger->decriticalbug($msg, $context);
+        if ( ! self::$logger ) {
+            self::init(null);
+        }
+        self::$logger->critical($msg, $context);
     }
 
     static public function alert($msg, array $context = [])
     {
+        if ( ! self::$logger ) {
+            self::init(null);
+        }
         self::$logger->alert($msg, $context);
     }
 
     static public function emergency($msg, array $context = [])
     {
+        if ( ! self::$logger ) {
+            self::init(null);
+        }
         self::$logger->emergency($msg, $context);
     }
 }
